0a6f9cbb04bcb6e4d0e3d73b6de968d37f542b73
Samuel <samuelou510@gmail.com>
2024-08-13 02:42:09 -0400

    timing fix

--- app/page.js
+++ app/page.js
@@ -50,7 +50,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 2000);
+    }, 2100);
     const finishedLoading = setTimeout(() => {
       setIsPageReady(true);
     }, 3500);
