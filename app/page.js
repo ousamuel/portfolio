f53cec9aa287d2a30bd065c63f4362b1624bedec
Samuel <samuelou510@gmail.com>
2024-08-17 18:45:53 -0400

    fixed gradient

--- app/page.js
+++ app/page.js
@@ -94,7 +94,7 @@ export default function Home() {
       {/* <div className={isVisible ? "see-more" : "hidden absolute"}>
         <img src="/images/see-more_white.svg" alt="▽" className="w-[40px]" />
       </div> */}
-      <main className="main-body background-gradient">
+      <main className="main-body ">
         <div className="buttons z-50 w-5/6 md:w-3/5 max-w-[500px]">
           {allButtons.map((btnName, i) => {
             return (
